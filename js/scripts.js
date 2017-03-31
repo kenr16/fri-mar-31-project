5182e1afd3f8c5e862317365319c8181bb8c5486
Ken Rutan <kenr16@gmail.com>
2017-03-31 15:11:37 -0700

    Add a random quote generator to the left hand box

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // *************************************************
 // Back End Code
 // *************************************************
+
 var pingPong = function(userInput) {
   var allNumbers = [];
   for (var i = 1; i <= userInput; i++) {        //This will start at 1 and cycle up to the user's input.
@@ -22,11 +23,12 @@ var disqualifyTest = function(toBeTested) {       //This number is a disqualifyi
 // *************************************************
 // Front End Code
 // *************************************************
+
 $(document).ready(function() {
   $(".formOne").submit(function(event) {
     event.preventDefault();
-    var inputText = $('input#uxInput').val();   //This will take the user's input and establish it as a variable.
-    if (inputText === "Beware of zombies") {
+    var inputText = $('input#uxInput').val().toUpperCase();   //This will take the user's input and establish it as a variable.
+    if (inputText === "BEWARE OF ZOMBIES") {    //Go ahead, type it into the input box... I dare you.
       var audio = new Audio('sound/zombie.mp3');
       audio.play();
       $(".zombie").show();
@@ -35,12 +37,16 @@ $(document).ready(function() {
         $(".zombie").hide();
       }, delayMillis);
     }
-    var inputNumber = parseInt(inputText);
-    var directionInput = $("input:radio[name=direction]:checked").val();
+    var inputNumber = parseInt(inputText);      //This will ensure that the user's input is in integer form for compatibility.
+    if (inputNumber > 10000) {
+      alert("Ask yourself if you REALLY *want* to see a list of numbers that long...");
+      return
+    }
+    var directionInput = $("input:radio[name=direction]:checked").val();  //This will return whichever direction the user wants the list to read.
     $("#displayHere").empty();                              //This line will clear the display every time the users presses submit.
     if (disqualifyTest(inputNumber) === true) {             //This will run a disqualifying test to make certain that the number put in was an actual 0-9 number.
-      var audio = new Audio('sound/pingpong.mp3');
-      audio.play();
+      var audio = new Audio('sound/pingpong.mp3');          //This line will establish a sound effect.
+      audio.play();                                         //This line will run the sound effect when the user hits submit.
       var finalPrintOut = pingPong(inputNumber);    //This will establish the output of the function as a variable at this end.
       if (directionInput === 'forward') {
         finalPrintOut.forEach(function(outputNumber) {        //This part goes through each item in the array and converts it into a list item.
@@ -48,7 +54,7 @@ $(document).ready(function() {
         });
       } else if (directionInput === 'reverse') {
         finalPrintOut.forEach(function(outputNumber) {        //This part goes through each item in the array and converts it into a list item.
-          $("#displayHere").prepend("<li>"+outputNumber+"</li>");
+          $("#displayHere").prepend("<li>"+outputNumber+"</li>"); //Instead of appending, we will prepend the list, such that it goes in reverse order.
         });
       }
     } else {
@@ -56,4 +62,21 @@ $(document).ready(function() {
       return
     }
   });
+
+  var randomNumber = Math.floor((Math.random() * 4) + 1);
+  if (randomNumber == 1) {
+    $("#quoteHere").append("Zombies are a lot like programmers, they love you for your brains.");
+    $("#authorHere").append("Anonymous");
+  } else if (randomNumber == 2) {
+    $("#quoteHere").append("Zombies love you for what's on the inside.");
+    $("#authorHere").append("Anonymous");
+  } else if (randomNumber == 3) {
+    $("#quoteHere").append("Something coming back from the dead was almost always bad news... for every one Jesus you get a million zombies.");
+    $("#authorHere").append("David Wong");
+  } else if (randomNumber == 4) {
+    $("#quoteHere").append("Zombies hate fast food.");
+    $("#authorHere").append("Anonymous");
+  }
+
+
 });
